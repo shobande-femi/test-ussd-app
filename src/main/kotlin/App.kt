f762d6de7e53abea3207cf2504b17b82fc983b37
Oluwafemi Shobande <oluwafemi.shobande@topia.com>
2020-02-19 14:02:41 +0200

    run one or the other

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,7 +1,9 @@
 suspend fun main() {
+     // Run one or the other
+
     //uncomment this to run Africas talking ussd app
      AfricasTalkingUssdApp.main()
 
     // uncomment this to run Hubtel ussd App
-    // HubTelUssdApp.main()
+     // HubTelUssdApp.main()
 }
